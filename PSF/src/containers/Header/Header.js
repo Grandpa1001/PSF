dfd3892354608153f8be4d09796fd37fa349d7eb
Kamil <kamil.bandzwolek@gmail.com>
2021-06-02 06:53:51 +0200

    2.4
    add hover MenuButton, isActive

--- PSF/src/containers/Header/Header.js
+++ PSF/src/containers/Header/Header.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
 import MenuButton from './components/MenuButton/MenuButton';
-
+import './styles/header.less';
 export default class Header extends Component {
 
   render (){
-
     return (
-       <div>
-        <ul>
-          <MenuButton>Portfolio</MenuButton>
+       <div className= "main-header">
+        <ul className = "main-header-menu">
+          <MenuButton isActive={true}>Portfolio</MenuButton>
           <MenuButton>Teksty</MenuButton>
           <MenuButton>Mordeczki</MenuButton>
         </ul>
