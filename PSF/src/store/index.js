cea34ba26dc164e3afdf473bbcc390fff2cc9cf7
Kamil <kamil.bandzwolek@gmail.com>
2021-06-05 11:34:47 +0200

    4.5
    redux-logger, log actions

--- PSF/src/store/index.js
+++ PSF/src/store/index.js
@@ -1,11 +1,18 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers/rootReducer';
+
+
 const mainStore = combineReducers(
   {
   root: rootReducer,
   }
 )
+const middlewares = [];
+if (process.env.NODE_ENV ===`development`){
+  const {createLogger} = require(`redux-logger`);
+  middlewares.push(createLogger({collapsed:true}));
+}
 
-const store = createStore(mainStore);
+const store = compose(applyMiddleware(...middlewares))(createStore)(mainStore);
 
 export default store;
